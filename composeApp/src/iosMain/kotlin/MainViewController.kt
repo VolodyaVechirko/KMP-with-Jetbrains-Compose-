72dd44a8aee99ea594c34e0b8ecbb3f994a50634
VolodymyrVechirko <volodya.vechirko@gmail.com>
2024-01-27 19:50:44 +0100

    Update ios MainViewController

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -6,21 +6,22 @@ import com.seiko.imageloader.LocalImageLoader
 import com.seiko.imageloader.createDefaultIOS
 import platform.UIKit.UIViewController
 
-@Suppress("FunctionName")
-fun MainViewController(): UIViewController = ComposeUIViewController {
-    CompositionLocalProvider(
-        LocalImageLoader provides remember { generateImageLoader() },
-    ) {
-        ComposeApp()
+class MainViewController {
+    fun get(): UIViewController = ComposeUIViewController {
+        CompositionLocalProvider(
+            LocalImageLoader provides remember { generateImageLoader() },
+        ) {
+            ComposeApp()
+        }
     }
-}
 
-/**
- * iOS ImageLoader cache config
- */
-private fun generateImageLoader(): ImageLoader {
-    return ImageLoader {
-        takeFrom(ImageLoader.createDefaultIOS())
-        commonConfig()
+    /**
+     * iOS ImageLoader cache config
+     */
+    private fun generateImageLoader(): ImageLoader {
+        return ImageLoader {
+            takeFrom(ImageLoader.createDefaultIOS())
+            commonConfig()
+        }
     }
 }
